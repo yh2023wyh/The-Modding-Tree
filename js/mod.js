902da4a3bd804838e5052374d1a9e68e58b11a03
yh2023wyh <592505318@qq.com>
2025-09-07 09:49:11 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,8 +43,8 @@ function getPointGen() {
 
 	let gain = new Decimal(0)
 	if(hasUpgrade("r",11)) gain = gain.add(128);
-	if(hasUpgrade("r",12)) gain = gain.times(upgradeEffect('p',12))
-	if(hasUpgrade("r",13)) gain = gain.times(upgradeEffect('p',13))
+	if(hasUpgrade("r",12)) gain = gain.times(upgradeEffect('p',12));
+	if(hasUpgrade("r",13)) gain = gain.times(upgradeEffect('p',13));
 	return gain
 }
 
